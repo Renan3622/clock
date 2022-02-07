e2c3c06098e4817543b747e411a10ee05b9ef48b
Renan Da Silva Souza <61859849+Renan3622@users.noreply.github.com>
2022-02-06 22:40:50 -0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package com.mycompany.clock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.mycompany.clock.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
